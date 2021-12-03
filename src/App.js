e3ee8060680aeea3b2e0a269277b2a7d1967a73f
Ravi Le <ravi.le@transwap.com>
2021-12-04 01:54:52 +0700

    update UI

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
 import './App.css';
-
-import HomePage from './pages/homepage/homepage.component';
+import HomePage from "./components/homepage.component";
 
 function App() {
   return (
-    <div>
-      <HomePage />
-    </div>
+      <div>
+        <HomePage />
+      </div>
+
   );
 }
 
